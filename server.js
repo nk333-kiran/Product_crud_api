4140b0efd63dec6f98764ed420393d3a5a61cac0
Navaneeth <mnavaneeth321@gmail.com>
2026-06-13 13:02:45 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const productRoutes = require("./routes/productRoutes");
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
+
+app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
